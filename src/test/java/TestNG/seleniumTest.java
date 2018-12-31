1ac8903bf8c525c67c1113f958e6b195afb97ce0
bunty2019 <46006430+bunty2019@users.noreply.github.com>
2018-12-31 14:58:15 -0500

    Update seleniumTest.java

--- src/test/java/TestNG/seleniumTest.java
+++ src/test/java/TestNG/seleniumTest.java
@@ -7,11 +7,13 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class seleniumTest {
 
@@ -19,10 +21,19 @@ public class seleniumTest {
 	WebDriver driver;
 	@BeforeMethod
 	public void launch() throws MalformedURLException {
-		  //System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		  System.setProperty("webdriver.gecko.driver","/Users/Singh/Selenium/geckodriver");
+		  System.setProperty("webdriver.chrome.driver", "/home/ubuntu/selenium-website/chromedriver");
+		  //System.setProperty("webdriver.gecko.driver","/Users/Singh/Selenium/geckodriver");
+		  
+		  
+		  ChromeOptions Options= new ChromeOptions();
+		  Options.addArguments("--headless");
+		  WebDriver driver= new ChromeDriver(Options);
+		  driver.get("http://ec2-3-82-212-169.compute-1.amazonaws.com:3001");
+		   
+		  /*
 		
-		  String URL = "http://192.168.1.169:3001";
+		  //String URL = "http://192.168.1.169:3001";
+		  String URL = "http://ec2-3-82-212-169.compute-1.amazonaws.com::3001";
 	 	  String Node = "http://192.168.1.169:4444/wd/hub";
 	 		
 	 	  //DesiredCapabilities cap = DesiredCapabilities.chrome();
@@ -34,14 +45,21 @@ public class seleniumTest {
 		  driver.get(URL);
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		  */
 		}
+	
 	@Test
 	public void checkLogin() throws InterruptedException {
 	  
-		 driver.findElement(By.name("login")).sendKeys("devops");
-	     Thread.sleep(100);
-	     driver.findElement(By.name("password")).sendKeys("test");
-	     Thread.sleep(100);
+		  driver.findElement(By.name("login")).sendKeys("devops");	
+		  driver.findElement(By.name("password")).sendKeys("test");
+		  driver.findElement(By.name("click")).click();
+		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		  String test= driver.findElement(By.name("test")).getText();
+		  
+		  assertEquals(test, "Login Succeeded");
+		  System.out.println("Test Succeeded!!");
+
 
 	 	  	  
 	}
